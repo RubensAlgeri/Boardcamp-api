c17233b7a99ab1e89771b40716583e333c4b81c4
Rubens Algeri <rubens_zys@hotmail.com>
2022-05-30 16:35:04 -0300

    remover arquivos desnecessarios

--- middlewares/rentalSchemaMiddleware.js
+++
@@ -1,9 +0,0 @@
-import rentalSchema from "../schemas/rentalSchema.js";
-
-export function validaAluguel (req, res, next) {
-    const validation = rentalSchema.validate(req.body);
-    if (validation.error) {
-        return res.status(400).send(validation.error.details.map(details=>details.message));
-    }
-    next()
-    }
--- routes/alugueisRouter.js
+++ routes/alugueisRouter.js
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import { buscarAlugueis, finalizarAluguel, inserirAluguel, removerAluguel} from '../controllers/alugueisController.js';
-import { validaAluguel } from '../middlewares/rentalSchemaMiddleware.js';
 
 const rentalsRouter = Router();
 rentalsRouter.get("/rentals", buscarAlugueis);
